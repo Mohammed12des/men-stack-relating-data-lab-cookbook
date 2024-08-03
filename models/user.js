823aca4614fc86981340b9212355a67ae3b1488a
Mohammed12des <hamood12des@gmail.com>
2024-08-03 10:11:44 +0300

    answer of the part one

--- models/user.js
+++ models/user.js
@@ -1,4 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
 
 const userSchema = mongoose.Schema({
   username: {
@@ -9,14 +16,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  pantry:[foodSchema]
-});
-const foodSchema = new mongoose.Schema({
-  name:{String,
-    required: true,
-  }
+  pantry: [foodSchema],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
